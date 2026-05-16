561e0f42eb7bf865eddb00e5780ba2bc768951ba
Atheer <atheeraladwan371@gmail.com>
2026-05-16 18:28:26 +0300

    allowd the vercel link othe branch of home page design

--- app/main.py
+++ app/main.py
@@ -20,6 +20,7 @@ app.add_middleware(
     "http://127.0.0.1:5174",
     "http://localhost:3000",
     "https://gp-frontend-gamma.vercel.app",
+    "https://gp-frontend-git-feature-ef5217-atheeraladwan371-7653s-projects.vercel.app",
 
     ],
     allow_credentials=True,
